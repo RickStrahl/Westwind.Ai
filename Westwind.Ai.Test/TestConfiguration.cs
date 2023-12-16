76bc88827e7205070023a93852145d612deac8da
Rick Strahl <rstrahl@west-wind.com>
2023-12-15 22:31:12 -1000

    Update test names

--- Westwind.Ai.Test/TestConfiguration.cs
+++ Westwind.Ai.Test/TestConfiguration.cs
@@ -1,9 +1,22 @@
+using System.Transactions;
+using Westwind.Utilities;
+
 namespace Westwind.Ai.Test;
 
 public class TestConfiguration
 {
+    static TestConfiguration()
+    {
+        Current =  JsonSerializationUtils.DeserializeFromFile<TestConfiguration>("__TestConfiguration-NoGit.json");
+    }
+
+    public static TestConfiguration Current { get; set; }
+
     public string OpenAiApiKey { get; set; }
 
     public string AzureConnectionString { get; set; }
     public string AzureApiKey { get; set; }
+
+
+    
 }
